a86d1ccfc519874002f83e123fa802d6dcde4d12
workramsah <workramsah@gmail.com>
2026-03-11 13:19:44 +0545

    fix error

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,11 +1,12 @@
 import Form from "../component/Form";
 import TestFun from "../component/TestFun";
+import View from "../component/View";
 
 export default function Page(){
    return(
       <>
       <div>hello world from test page</div>
-      <TestFun/>
+      <View/>
       </>
    )
 }
